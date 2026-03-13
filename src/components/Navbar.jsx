024e88e4b199d1c16744437245dccf43f21166af
jyanza63 <jyanza63@gmail.com>
2026-03-13 14:30:02 -0400

    Add Navbar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="logo">JY</div>
+      <nav>
+        <a href="#features">Features</a>
+        <a href="#stats">Stats</a>
+        <a href="#contact" className="btn">Contact</a>
+      </nav>
+    </header>
+  );
+}
