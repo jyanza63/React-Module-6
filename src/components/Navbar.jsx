48cb6e5e6ae4fddff0e97c13b92066a25f343fac
jyanza63 <jyanza63@gmail.com>
2026-03-14 10:11:02 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-export default function Navbar() {
+import { Link } from "react-router-dom";
+
+export default function Navbar({ user }) {
   return (
-    <header className="navbar">
-      <div className="logo">JY</div>
-      <nav>
-        <a href="#features">Features</a>
-        <a href="#stats">Stats</a>
-        <a href="#contact" className="btn">Contact</a>
-      </nav>
-    </header>
+    <nav className="navbar">
+      <div className="nav-left">
+        <span className="brand">Juan Yanza React Final</span>
+      </div>
+      <div className="nav-right">
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        {!user && <Link to="/login">Login</Link>}
+        {user && <span className="user-pill">Hi, {user.firstName}</span>}
+      </div>
+    </nav>
   );
 }
