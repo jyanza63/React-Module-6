162dd714d8c4ca95f7d6ed2240c64c2207bc275f
jyanza63 <jyanza63@gmail.com>
2026-03-15 19:00:46 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,4 @@
-import Projects from "./components/Projects";
-
-export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Stats />
-      <Projects />
-      <CTA />
-      <Footer />
-    </>
-  );
-}
-
+import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
