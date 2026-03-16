28521145d045c95ef944075560622662f7146213
jyanza63 <jyanza63@gmail.com>
2026-03-15 20:05:46 -0400

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,1 +1,12 @@
 import ProtectedRoute from "./components/ProtectedRoute";
+
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const isAuthenticated = localStorage.getItem("auth") === "true";
+
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+}
+
+export default ProtectedRoute;
+
